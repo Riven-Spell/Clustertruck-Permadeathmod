292a584735b1bf57381e2692e7ac538bf30a1363
Adele Reed <privacyradiator@gmail.com>
2016-05-23 21:18:04 -0700

    Initial commit

---
+++ PermadeathMod/Main.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+namespace Mod
+{
+    public class Main : MonoBehaviour
+    {
+        public void Start()
+        {
+            var modObject = new GameObject();
+            modObject.AddComponent<modComponent>();
+            DontDestroyOnLoad(modObject);
+        }
+    }
+
+
+    public class modComponent : MonoBehaviour
+    {
+        GameManager gm;
+        Manager m;
+        public void Start() {
+            gm = FindObjectOfType<GameManager>();
+            m = FindObjectOfType<Manager>();
+        }
+
+        public void Update() {
+            if (GameManager.mapDeaths != 0)
+            {
+                m.Play(true);
+                GameManager.mapDeaths = 0;
+            }
+            if(TesseractModLoader.)
+        }
+    }
+}
